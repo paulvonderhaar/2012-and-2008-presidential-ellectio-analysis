5bd39b311e7e211abc20d6ee1ed942074c513d3f
unknown <pvonhaar@gmail.com>
2015-10-15 12:52:01 -0500

    finally managed to get the data into a useable form, uploading it now I'll try to have the data wrangled by class, we can talk about what we're gonna do then

---
+++ 01 Data/LoadingDataFrames.R
@@ -0,0 +1,8 @@
+require("jsonlite")
+require("RCurl")
+df1 <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from ELECTIONOF2008 where STATE is not null"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_pmv347', PASS='orcl_pmv347', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+head(df1)
+
+df2 <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from ELECTIONOF2012 where STATE is not null"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_pmv347', PASS='orcl_pmv347', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+head(df2)
+df2
